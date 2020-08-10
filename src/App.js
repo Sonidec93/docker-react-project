34d513f85d189d26ddbd0b9272d1917ca13ce656
Sonidec93 <mukul.kumra@gmail.com>
2020-08-11 01:07:04 +0530

    changed text to test pull request with travis

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         Learn react
+         Learning react
         </a>
       </header>
     </div>
